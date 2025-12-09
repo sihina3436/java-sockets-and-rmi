0dc434ad356effea577a51b8499c3b120a290206
sihina3436 <nimnadasihina@gmail.com>
2025-12-09 15:35:21 +0530

    Impliment Calculator interface

---
+++ src/RMI_Calculator/Calculator.java
@@ -0,0 +1,12 @@
+package RMI_Calculator;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Calculator extends Remote {
+    double add(double a, double b) throws RemoteException;
+    double subtract(double a, double b) throws RemoteException;
+    double multiply(double a, double b) throws RemoteException;
+    double divide(double a, double b) throws RemoteException;
+
+}
